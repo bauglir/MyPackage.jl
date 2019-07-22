9bb9c0f77e7ae447082d7b5cdd8c971fe42e5279
Joris Kraak <me@joriskraak.nl>
2019-07-22 14:38:41 -0400

    feat: add `euclidean` function

--- src/MyPackage.jl
+++ src/MyPackage.jl
@@ -1,2 +1,15 @@
 module MyPackage
+
+using Distances
+
+"""
+    euclidian(x, y)
+
+Evaluate the euclidian distance.
+"""
+function euclidean(x, y)
+  r = evaluate(Euclidean(), x, y)
+  return r
+end
+
 end
