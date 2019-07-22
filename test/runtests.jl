0fc0bd387a2fcdd9726eaaee8aab2e573ae6d69e
Joris Kraak <me@joriskraak.nl>
2019-07-22 15:01:39 -0400

    test: add basic tests

---
+++ test/runtests.jl
@@ -0,0 +1,13 @@
+module MyPackageTest
+
+using MyPackage
+using Test: @test, @testset
+
+@testset "MyPackage" begin
+  x, y = rand(4), rand(4)
+
+  @test MyPackage.euclidean(x, x) == 0
+  @test MyPackage.euclidean(x, y) ≈ sqrt(sum(abs2, x.-y))
+end
+
+end
